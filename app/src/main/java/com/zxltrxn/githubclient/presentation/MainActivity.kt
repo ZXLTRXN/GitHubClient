96d022c62c98e0bee3e994b7bd572da1991d2df2
Ilya Shevtsov <ishevtsov@icerockdev.com>
2022-05-13 15:44:14 +0700

    removed splashFragment

--- app/src/main/java/com/zxltrxn/githubclient/presentation/MainActivity.kt
+++ app/src/main/java/com/zxltrxn/githubclient/presentation/MainActivity.kt
@@ -3,41 +3,55 @@ package com.zxltrxn.githubclient.presentation
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.zxltrxn.githubclient.R
-import com.zxltrxn.githubclient.data.Resource
-import com.zxltrxn.githubclient.data.network.APIService.Companion.WRONG_TOKEN_CODE
 import com.zxltrxn.githubclient.data.repository.IAuthRepository
-import com.zxltrxn.githubclient.presentation.splash.SplashFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    val IS_ENTERED_KEY = "isEntered"
 
     @Inject
     lateinit var authRepo: IAuthRepository
+
     private val navController: NavController by lazy { getNavigationController() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (1 == 1) {
+                        // The content is ready; start drawing.
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        // The content is not ready; suspend.
+                        false
+                    }
+                }
+            }
+        )
+
         setSupportActionBar(findViewById(R.id.my_toolbar))
 
-        val isEntered = savedInstanceState?.getBoolean(IS_ENTERED_KEY)
-        if (isEntered == null || isEntered == false) authenticationWithRouting()
-    }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        if (navController.currentDestination != navController.findDestination(R.id.splashFragment)) {
-            outState.putBoolean(IS_ENTERED_KEY, true)
-        }
-        super.onSaveInstanceState(outState)
     }
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        if (navController.currentDestination != navController.findDestination(R.id.splashFragment)) {
+//            outState.putBoolean(IS_ENTERED_KEY, true)
+//        }
+//        super.onSaveInstanceState(outState)
+//    }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
@@ -67,31 +81,25 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     private fun authenticationWithRouting() {
-        lifecycleScope.launch {
-            when (val res = authRepo.signIn()) {
-                is Resource.Success -> {
-                    navigateToRepositoriesList()
-                }
-                is Resource.Error -> {
-                    if (res.code == WRONG_TOKEN_CODE) {
-                        navigateToAuth()
-                    } else {
-                        navigateToRepositoriesList()
-                    }
-                }
-            }
-        }
+//        lifecycleScope.launch {
+//            when (val res = authRepo.signIn()) {
+//                is Resource.Success -> {
+//                    navigateToRepositoriesList()
+//                }
+//                is Resource.Error -> {
+//                    if (res.code == WRONG_TOKEN_CODE) {
+//                        navigateToAuth()
+//                    } else {
+//                        navigateToRepositoriesList()
+//                    }
+//                }
+//            }
+//        }
     }
 
     private fun navigateToRepositoriesList() {
-        val action = SplashFragmentDirections
-            .splashFragmentToRepositoriesListFragment()
-        navController.navigate(action)
-    }
-
-    private fun navigateToAuth() {
-        val action = SplashFragmentDirections
-            .splashFragmentToAuthFragment()
-        navController.navigate(action)
+//        val action = SplashFragmentDirections
+//            .splashFragmentToRepositoriesListFragment()
+//        navController.navigate(action)
     }
 }
