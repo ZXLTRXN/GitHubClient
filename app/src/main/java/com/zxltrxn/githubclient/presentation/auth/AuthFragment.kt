3d01e56a044010be45beef9f037cd42045b4fd96
Ilya Shevtsov <ishevtsov@icerockdev.com>
2022-05-13 11:55:00 +0700

    removed dataBinding from AuthFragment

--- app/src/main/java/com/zxltrxn/githubclient/presentation/auth/AuthFragment.kt
+++ app/src/main/java/com/zxltrxn/githubclient/presentation/auth/AuthFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -12,7 +12,10 @@ import com.zxltrxn.githubclient.R
 import com.zxltrxn.githubclient.databinding.FragmentAuthBinding
 import com.zxltrxn.githubclient.presentation.MainActivity
 import com.zxltrxn.githubclient.presentation.auth.AuthViewModel.Action
+import com.zxltrxn.githubclient.presentation.auth.AuthViewModel.State
+import com.zxltrxn.githubclient.utils.collectLatestLifecycleFlow
 import com.zxltrxn.githubclient.utils.collectLifecycleFlow
+import com.zxltrxn.githubclient.utils.showToast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,12 +32,8 @@ class AuthFragment : Fragment(R.layout.fragment_auth) {
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentAuthBinding.inflate(inflater, container, false)
-        val view = binding.apply {
-            lifecycleOwner = viewLifecycleOwner
-            vm = viewModel
-        }
         (requireActivity() as MainActivity).supportActionBar?.hide()
-        return view.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,10 +47,24 @@ class AuthFragment : Fragment(R.layout.fragment_auth) {
     }
 
     private fun observe() {
+        collectLatestLifecycleFlow(viewModel.state) { state ->
+            binding.inputLayout.error = if (state is State.InvalidInput) state.reason else null
+            binding.progressCircular.visibility =
+                if (state is State.Loading) View.VISIBLE else View.GONE
+            binding.submitButton.isEnabled = state !is State.Loading
+            binding.submitButton.setOnClickListener { viewModel.onSignButtonPressed() }
+            binding.submitButton.text =
+                if (state is State.Loading) "" else getString(R.string.btn_sign_in)
+        }
+
+        binding.inputEditText.doAfterTextChanged {
+            viewModel.token.value = it.toString()
+        }
+
         collectLifecycleFlow(viewModel.actions) { action ->
             when (action) {
                 is Action.ShowError ->
-                    Toast.makeText(requireContext(), action.message, Toast.LENGTH_SHORT).show()
+                    showToast(action.message)
                 is Action.RouteToMain -> navigateToRepositoriesList()
             }
         }
@@ -62,4 +75,6 @@ class AuthFragment : Fragment(R.layout.fragment_auth) {
             .authFragmentToRepositoriesListFragment()
         this.findNavController().navigate(action)
     }
+
+
 }
